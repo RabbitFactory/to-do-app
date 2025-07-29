5b96da0f7d7e6d49e31bfc5c2101fe3fa49b5f53
Rabbit Factory <jv16th.raj@gmail.com>
2025-07-30 01:32:40 +0600

    good progress on styling so far

--- script.js
+++ script.js
@@ -4,24 +4,40 @@ const list = document.getElementById("list");
 
 const addingFunc = () => {
   const task = input.value.trim();
-  if(task == "") return; 
+  if (task == "") return;
 
   const li = document.createElement("li");
+  li.classList.add("task-item");
+
+  const content = document.createElement("div");
+  content.classList.add("task-content");
+
+  const tickIcon = document.createElement("img");
+  tickIcon.src = "img/dry-clean.png";
+  tickIcon.className = "tickIcon";
+
+  const taskText = document.createElement("span");
+  taskText.textContent = task;
+
   const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = `<img src="img/close.png" alt="Delete" class="deleteIcon" />`;
 
-  li.textContent = task;
-  deleteButton.textContent = "X";
+  content.appendChild(tickIcon);
+  content.appendChild(taskText);
 
-  list.appendChild(li);
+  li.appendChild(content);
   li.appendChild(deleteButton);
+  list.appendChild(li);
 
-  li.addEventListener("click", () => {
-    li.classList.toggle('done');
-  })
+   li.addEventListener("click", () => {
+    li.classList.toggle("done");
 
-  deleteButton.addEventListener('click', () => {
+    tickIcon.src = li.classList.contains("done") ? "img/checked.png" : "img/dry-clean.png";
+  });
+
+  deleteButton.addEventListener("click", () => {
     list.removeChild(li);
-  })
+  });
 
   input.value = "";
 };
